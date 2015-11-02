e0c1bc8e81618a90c72a3b580c93ec3756e8c48c
Maksim <mksmshevchenko@gmail.com>
2015-11-02 04:51:38 -0500

    Fixed the DB connection

--- config/database.php
+++ config/database.php
@@ -27,7 +27,7 @@ return [
     */
 
     //'default' => env('DB_CONNECTION', 'mysql'),
-	'default' => 'sqlite',
+	'default' => 'pgsql',
 
     /*
     |--------------------------------------------------------------------------
@@ -64,13 +64,13 @@ return [
             'prefix'    => '',
             'strict'    => false,
         ],
-
+        
         'pgsql' => [
             'driver'   => 'pgsql',
-            'host'     => env('DB_HOST', 'localhost'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host'     => parse_url(getenv("DATABASE_URL"))["host"],
+            'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
+            'username' => parse_url(getenv("DATABASE_URL"))["user"],
+            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
