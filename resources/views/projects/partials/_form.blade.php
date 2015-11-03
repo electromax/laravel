eace822edd078a7b67beaf8d1b8e0629b390548b
Maksim <mksmshevchenko@gmail.com>
2015-11-03 13:59:46 -0500

    Improved the style of edit pages

--- resources/views/projects/partials/_form.blade.php
+++ resources/views/projects/partials/_form.blade.php
@@ -1,11 +1,13 @@
-<div class="row">
-    {!! Form::label('name', 'Name:', array('class' => 'col-sm-2')) !!}
-    {!! Form::text('name', null, array('class' => 'col-sm-10')) !!}
-</div>
-<div class="row">
-    {!! Form::label('slug', 'Slug:', array('class' => 'col-sm-2')) !!}
-    {!! Form::text('slug', null, array('class' => 'col-sm-10')) !!}
-</div>
-<div class="row">
-    {!! Form::submit($submit_text, ['class'=>'btn btn-primary']) !!}
+<div class="container">
+	<div class="row">
+	    {!! Form::label('name', 'Name:', array('class' => 'col-sm-2')) !!}
+	    {!! Form::text('name', null, array('class' => 'col-sm-10')) !!}
+	</div>
+	<div class="row">
+	    {!! Form::label('slug', 'Slug:', array('class' => 'col-sm-2')) !!}
+	    {!! Form::text('slug', null, array('class' => 'col-sm-10')) !!}
+	</div>
+	<div class="row">
+	    {!! Form::submit($submit_text, ['class'=>'btn btn-primary']) !!}
+	</div>
 </div>
--- resources/views/tasks/partials/_form.blade.php
+++ resources/views/tasks/partials/_form.blade.php
@@ -1,23 +1,25 @@
-<div class="row">
-    {!! Form::label('name', 'Name:', array('class' => 'col-sm-2')) !!}
-    {!! Form::text('name', null, array('class' => 'col-sm-10')) !!}
-</div>
+<div class="container">
+	<div class="row">
+	    {!! Form::label('name', 'Name:', array('class' => 'col-sm-2')) !!}
+	    {!! Form::text('name', null, array('class' => 'col-sm-10')) !!}
+	</div>
 
-<div class="row">
-    {!! Form::label('slug', 'Slug:', array('class' => 'col-sm-2')) !!}
-    {!! Form::text('slug', null, array('class' => 'col-sm-10')) !!}
-</div>
+	<div class="row">
+	    {!! Form::label('slug', 'Slug:', array('class' => 'col-sm-2')) !!}
+	    {!! Form::text('slug', null, array('class' => 'col-sm-10')) !!}
+	</div>
 
-<div class="row">
-    {!! Form::label('completed', 'Completed:', array('class' => 'col-sm-2')) !!}
-    {!! Form::checkbox('completed', null, array('class' => 'col-sm-10')) !!}
-</div>
+	<div class="row">
+	    {!! Form::label('completed', 'Completed:', array('class' => 'col-sm-2')) !!}
+	    {!! Form::checkbox('completed', null, array('class' => 'col-sm-10')) !!}
+	</div>
 
-<div class="row">
-    {!! Form::label('description', 'Description:', array('class' => 'col-sm-2')) !!}
-    {!! Form::textarea('description', null, array('class' => 'col-sm-10')) !!}
-</div>
+	<div class="row">
+	    {!! Form::label('description', 'Description:', array('class' => 'col-sm-2')) !!}
+	    {!! Form::textarea('description', null, array('class' => 'col-sm-10')) !!}
+	</div>
 
-<div class="row">
-    {!! Form::submit($submit_text, ['class'=>'btn btn-primary']) !!}
+	<div class="row">
+	    {!! Form::submit($submit_text, ['class'=>'btn btn-primary']) !!}
+	</div>
 </div>
